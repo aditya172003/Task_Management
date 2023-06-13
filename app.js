d5e394d5bd87f928e1dde47bbce507296cbbb0e6
Aditya Mahajan <adityam172003@gmail.com>
2023-06-14 03:07:10 +0530

    cors allowed

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const express = require("express");
 const app     = express();
 
 app.use(express.json());
-
-
+const cors = require('cors');
+app.use(cors());
 const dotenv=require("dotenv");
 dotenv.config();
 const Port = process.env.PORT;
